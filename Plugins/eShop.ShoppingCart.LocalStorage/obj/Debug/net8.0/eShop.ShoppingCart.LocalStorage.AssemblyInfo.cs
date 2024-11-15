0c72cfc79b967cbcc7a96c06f980c10251fdd684
Mohammed Mohalil Naikwadi <mohalil.naikwadi@agilepoint.com>
2024-11-15 20:43:00 +0530

    Second commit

--- Plugins/eShop.ShoppingCart.LocalStorage/obj/Debug/net8.0/eShop.ShoppingCart.LocalStorage.AssemblyInfo.cs
+++ Plugins/eShop.ShoppingCart.LocalStorage/obj/Debug/net8.0/eShop.ShoppingCart.LocalStorage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eShop.ShoppingCart.LocalStorage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c9d22e9a0f3f8dee13f58edf4682bdad07c20b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("eShop.ShoppingCart.LocalStorage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eShop.ShoppingCart.LocalStorage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
